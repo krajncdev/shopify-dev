e6d8b0e4a42e2c6a1213534798861f94f4aa419b
Jaka Krajnc <jaka.krajnc.jk@gmail.com>
2023-04-20 21:31:41 +0200

    Fix

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -15,34 +15,20 @@ const Image = ({
 }: ImagePropsI) => {
   return (
     <div className={`${customStyle}`}>
-      {imgBreakPoint === 1024 && (
-        <>
-          <img
-            src={imgSrc}
-            alt={alt}
-            className='h-full w-full object-cover image-pixelated lg:hidden'
-          />
-          <img
-            src={largeImgSrc}
-            alt={alt}
-            className='h-full w-full object-cover image-pixelated hidden lg:block'
-          />
-        </>
-      )}
-      {imgBreakPoint === 1280 && (
-        <>
-          <img
-            src={imgSrc}
-            alt={alt}
-            className='h-full w-full object-cover image-pixelated xl:hidden'
-          />
-          <img
-            src={largeImgSrc}
-            alt={alt}
-            className='h-full w-full object-cover image-pixelated hidden xl:block'
-          />
-        </>
-      )}
+      <img
+        src={imgSrc}
+        alt={alt}
+        className={`h-full w-full object-cover image-pixelated ${
+          imgBreakPoint === 1024 ? 'lg:hidden' : 'xl:hidden'
+        }`}
+      />
+      <img
+        src={largeImgSrc}
+        alt={alt}
+        className={`h-full w-full object-cover image-pixelated hidden ${
+          imgBreakPoint === 1024 ? 'lg:block' : 'xl:block'
+        }`}
+      />
     </div>
   );
 };
