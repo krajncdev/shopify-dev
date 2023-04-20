f0cae6ae66602daaebdbc0232433a1706a530274
Jaka Krajnc <jaka.krajnc.jk@gmail.com>
2023-04-20 20:30:51 +0200

    Fix

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -13,13 +13,27 @@ const Image = ({
   largeImgSrc,
   imgBreakPoint,
 }: ImagePropsI) => {
+  let mode;
+  switch (imgBreakPoint) {
+    case 1024:
+      mode = 'lg';
+      break;
+    case 1280:
+      mode = 'xl';
+      break;
+  }
+
   return (
     <div className={`${customStyle}`}>
       <img
         src={imgSrc}
-        srcSet={`${imgSrc} ${imgBreakPoint - 1}px, ${largeImgSrc}`}
         alt={alt}
-        className='h-full w-full object-cover image-pixelated'
+        className={`h-full w-full object-cover image-pixelated ${mode}:hidden`}
+      />
+      <img
+        src={largeImgSrc}
+        alt={alt}
+        className={`h-full w-full object-cover image-pixelated ${mode}:block hidden`}
       />
     </div>
   );
